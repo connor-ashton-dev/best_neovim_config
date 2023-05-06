39b8674e2a1bff177f60c9daae0dfcae2eae13f4
Connor Ashton <cjconnorashton@gmail.com>
2023-05-05 20:11:55 -0600

    took off autocomplete with copilot

--- after/plugin/completions.lua
+++ after/plugin/completions.lua
@@ -36,6 +36,10 @@ cmp.setup({
 			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 		end,
 	},
+	completion = {
+		autocomplete = false,
+	},
+
 	mapping = {
 		["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-n>"] = cmp.mapping.select_next_item(),
