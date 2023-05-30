e909fc493e593fa18d7f3d364c718a1a95dbe83c
Connor Ashton <cjconnorashton@gmail.com>
2023-05-30 11:54:09 -0600

    system clipboard

--- lua/connor/set.lua
+++ lua/connor/set.lua
@@ -5,6 +5,9 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.g.editorconfig = true
 
+--sets clipboard to system
+vim.api.nvim_set_option("clipboard", "unnamed")
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
