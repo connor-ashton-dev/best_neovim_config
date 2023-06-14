25a46cebd911c43f9dbd82818af64e24f511d0fd
Connor Ashton <cjconnorashton@gmail.com>
2023-06-14 16:04:25 -0600

    changed windows to open on top

--- lua/connor/set.lua
+++ lua/connor/set.lua
@@ -15,7 +15,7 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
-vim.opt.splitbelow = true
+vim.opt.splitbelow = false
 vim.opt.splitright = true
 
 vim.opt.fillchars:append({ eob = " " })
